4b58b59349c5b3afbcb01f484dc4683d303bd992
shahil <shxxhil.07@gmail.com>
2026-04-28 10:21:00 +0530

    feat: scaffold CountryPeek with routing, Header, and Home shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home";
-import NotFound from "./NotFound";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Header will be visible on all pages */}
       <Header />
-
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/country/:name" element={<div>Country Page</div>} />
-          <Route path="/favourites" element={<div>Favourites Page</div>} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/country/:name" element={<div>Country Page Placeholder</div>} />
+          <Route path="/favourites" element={<div>Favourites Page Placeholder</div>} />
+          <Route path="*" element={<NotFound />} />  //add//
         </Routes>
       </main>
     </BrowserRouter>
